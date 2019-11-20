8f6b2eb5589856ee6b8520956ae4dae73c25fa72
fernando <fernancumez@gmail.com>
2019-11-20 10:22:32 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './components/About';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<About/>, document.getElementById('root'));
